4b9e0220ee772c1a3bfc2ab586ef2a0924dcf57a
Alex Gadaix <alex.gadaix@gmail.com>
2021-03-30 14:09:56 +0100

    adding conditional messaging

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ const http = require('http');
 const moment = require('moment');
 
 const serverCallBack = (req, res) => {
-	const start_time = moment().format('9:00', 'HH:mm');
-	const finish_time = moment().format('18:00', 'HH:mm');
+	const start_time = moment('9:00', 'HH:mm');
+	const finish_time = moment('18:00', 'HH:mm');
 	const now = moment().format('HH:mm');
+	const start_diff = start_time.diff(moment(), 'minutes');
+	const end_diff = moment().diff(finish_time, 'minutes');
+	console.log(start_diff);
+	console.log(end_diff);
+	let additionalMessage = null;
+	if (start_diff > 0) {
+		additionalMessage = `Please contact me in ${start_diff} minutes`;
+	}
+	if (end_diff > 0) {
+		additionalMessage += `\nPlease contact me tomorrow.`;
+	}
 	const message = `
 	<html>
 		<head>
@@ -41,7 +52,10 @@ const serverCallBack = (req, res) => {
 				<h1>Hello everyone,</h1>
 				<h2>Welcome to our page.</h2>
 				<h3>Now, it\'s ${now}.</h3>
-				<h3>I am starting to work from ${start_time} to ${finish_time} Monday to Friday.
+				<h3>I am starting to work from ${start_time.format(
+					'HH:mm'
+				)} to ${finish_time.format('HH:mm')} Monday to Friday.</h3>
+				<p>${additionalMessage}</p>
 			</main>
 		</body>
 	</html>
