f562d945868980207655a316d04101f31f48a64a
Alex Gadaix <alex.gadaix@gmail.com>
2021-03-31 17:11:04 +0100

    get new route

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-console.log('hello');
 const http = require('http');
 const moment = require('moment');
 
 const serverCallBack = (req, res) => {
+	const url = req.url;
 	const start_time = moment('9:00', 'HH:mm');
 	const finish_time = moment('18:00', 'HH:mm');
 	const now = moment().format('HH:mm');
 	const start_diff = start_time.diff(moment(), 'minutes');
 	const end_diff = moment().diff(finish_time, 'minutes');
-	console.log(start_diff);
-	console.log(end_diff);
-	let additionalMessage = null;
+
+	let additionalMessage = '';
 	if (start_diff > 0) {
-		additionalMessage = `Please contact me in ${start_diff} minutes`;
+		additionalMessage = `Please contact me in ${start_diff} minutes.\n`;
 	}
 	if (end_diff > 0) {
-		additionalMessage += `\nPlease contact me tomorrow.`;
+		additionalMessage += `Please contact me tomorrow.`;
 	}
 	const message = `
 	<html>
@@ -55,13 +54,54 @@ const serverCallBack = (req, res) => {
 				<h3>I am starting to work from ${start_time.format(
 					'HH:mm'
 				)} to ${finish_time.format('HH:mm')} Monday to Friday.</h3>
-				<p>${additionalMessage}</p>
+				${additionalMessage && `<p>${additionalMessage}</p>`}
+				<p>Consult <a href="/list-uni-uk">list</a> of universities in UK</p>
+
 			</main>
 		</body>
 	</html>
 	`;
 
 	res.writeHead('200', { 'Content-Type': 'text/html' });
+	if (url === '/list-uni-uk') {
+		const page = `<html>
+		<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<style>
+			* {
+				box-sizing: border-box;
+			}
+			html {
+				font-size: 100%;
+			}
+			body {
+				background-color: ivory;
+				font-family: Helvetica, Arial, sans-serif;
+				
+			}
+			main {
+				padding: 20px;
+				width: 100%
+				max-width: 960px;
+				margin: 20px auto;
+				background-color: midnightblue;
+				color: white;
+			}
+			h1,h2,h3 {
+				font-weight: normal;
+			}
+
+		</style>
+		</head>
+		<body>
+			<main>	
+				<h1>Here is hte list of universities in UK.</h1>
+			</main>
+		</body>
+	</html>`;
+		res.write(page);
+		res.end();
+	}
 	res.end(message);
 };
 
